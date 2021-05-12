998d69ca467b6802a9576a309e51d0f298d93494
Andres Patiño <andresp199519@gmail.com>
2021-05-12 11:05:33 +0200

    Se creó la rama dev y se quitaron los comentarios que traía la plantilla.

--- src/view/index.php
+++ src/view/index.php
@@ -11,13 +11,12 @@
 
     <title>SB Admin 2 - Dashboard</title>
 
-    <!-- Custom fonts for this template-->
+
     <link href="../../vendorJs/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
-    <!-- Custom styles for this template-->
     <link href="../../css/sb-admin-2.min.css" rel="stylesheet">
     <link href="../../vendorJs/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
@@ -26,22 +25,16 @@
 <body id="page-top">
 
     <body id="page-top">
-        <!-- Page Wrapper -->
+
         <div id="wrapper">
 
 
-
-            <!-- Content Wrapper -->
             <div id="content-wrapper" class="d-flex flex-column">
 
-                <!-- Main Content -->
                 <div id="content">
 
-
-                    <!-- Begin Page Content -->
                     <div class="container-fluid">
 
-                                        <!-- DataTales Example -->
                         <div class="card shadow mb-4">
                             <div class="card-header py-3">
                                 <h1 class="m-0 font-weight-bold text-primary titulo-pagina ">Task appointment</h1>
@@ -112,12 +105,9 @@
                         </div>
 
                     </div>
-                    <!-- /.container-fluid -->
 
                 </div>
-                <!-- End of Main Content -->
 
-                <!-- Footer -->
                 <footer class="sticky-footer bg-white">
                     <div class="container my-auto">
                         <div class="copyright text-center my-auto">
@@ -125,38 +115,29 @@
                         </div>
                     </div>
                 </footer>
-                <!-- End of Footer -->
+ 
 
             </div>
-            <!-- End of Content Wrapper -->
 
         </div>
-        <!-- End of Page Wrapper -->
 
-        <!-- Scroll to Top Button-->
         <a class="scroll-to-top rounded" href="#page-top">
             <i class="fas fa-angle-up"></i>
         </a>
 
 
-
-        <!-- Bootstrap core JavaScript-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8"
             crossorigin="anonymous"></script>
         <script src="../../vendorJs/jquery/jquery.min.js"></script>
         <script src="../../vendorJs/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-        <!-- Core plugin JavaScript-->
+
         <script src="../../vendorJs/jquery-easing/jquery.easing.min.js"></script>
 
-        <!-- Custom scripts for all pages-->
+
         <script src="../../js/sb-admin-2.min.js"></script>
 
-        <!-- Page level plugins -->
-
-
-        <!-- Page level custom scripts -->
 
         <script src="../../vendorJs/datatables/jquery.dataTables.min.js"></script>
         <script src="../../vendorJs/datatables/dataTables.bootstrap4.min.js"></script>
